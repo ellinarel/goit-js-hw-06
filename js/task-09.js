0141da831439e02a0382d60bcfebd0dbb7352db9
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-02-06 00:05:12 +0200

    task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const textcolor = document.querySelector(".color")
+const changecolor = document.querySelector(".change-color")
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return document.body.style.background = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
+
+changecolor.addEventListener("click", ()=>{
+  document.body.style.backgroundColor = getRandomHexColor();
+  textcolor.textContent =  document.body.style.backgroundColor
+}
+)
